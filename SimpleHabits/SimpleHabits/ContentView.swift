fc2857b4225745261f60ce762a67ab4b9b42d435
palant-dev <antonio.palomba2015@gmail.com>
2023-10-07 22:25:44 +0200

    Adds ForYou view and move related elements to the appropriate file

--- SimpleHabits/SimpleHabits/ContentView.swift
+++ SimpleHabits/SimpleHabits/ContentView.swift
@@ -10,35 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            HStack {
-                Circle()
-                    .frame(width: 36, height: 36, alignment: .center)
-                    .foregroundStyle(.accountButtonIcon)
-                    .overlay {
-                        Image(systemName: "person")
-                            .foregroundStyle(.white)
-                    }
-                    .padding()
-                Spacer()
-            }
-
-            Spacer()
-
-            HStack {
-                Text("Continue Listening")
-                    .foregroundStyle(.white).bold()
-                    .padding()
-                Spacer()
-            }
-
-            RoundedRectangle(cornerSize: CGSize(width: 8, height: 8), style: .continuous)
-                .foregroundStyle(.gray)
-                .padding(.horizontal)
-
-            RoundedRectangle(cornerSize: CGSize(width: 8, height: 8), style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
-                .foregroundStyle(.wellnessScoreCardbg)
-                .padding(.horizontal)
-
+            ForYou()
         }
         .background {
             Color(.background).ignoresSafeArea()
