2263099f577f84035cea5acb154fe79510af4f71
palant-dev <antonio.palomba2015@gmail.com>
2023-10-08 10:43:56 +0200

    Adds an enum that manges the view selection for the tabbar

--- SimpleHabits/SimpleHabits/ContentView.swift
+++ SimpleHabits/SimpleHabits/ContentView.swift
@@ -8,48 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var selectedTabbarItem = "For You"
+    @State var selectedTabbarItem: SimpleHabitsViews = .forYou
     var body: some View {
         VStack {
-
-
             TabView(selection: $selectedTabbarItem) {
                 ForYouView()
                     .tabItem {
-                        VStack{
-                            Image(systemName: "play")
-                            Text("For you")
-                        }
-
+                        Label(SimpleHabitsViews.forYou.rawValue, systemImage: "play")
                     }
 
                 MeditateView()
                     .tabItem {
-                        VStack{
-                            Image(systemName: "circle")
-                            Text("For you")
-                        }
+                        Label(SimpleHabitsViews.meditate.rawValue, systemImage: "circle")
                     }
 
                 SleepView()
                     .tabItem {
-                        VStack{
-                            Image(systemName: "moon")
-                            Text("For you")
-                        }
+                        Label(SimpleHabitsViews.sleep.rawValue, systemImage: "moon")
                     }
 
                 SearchView()
                     .tabItem {
-                        VStack{
-                            Image(systemName: "play")
-                            Text("For you")
-                        }
+                        Label(SimpleHabitsViews.search.rawValue, systemImage: "play")
                     }
             }
             .toolbarBackground(Color.black, for: .tabBar)
         }
     }
+}
+
+enum SimpleHabitsViews: String {
+    case forYou = "For You",
+         meditate = "Meditate",
+         sleep = "Sleep",
+         search = "Search"
 }
 
 #Preview {
